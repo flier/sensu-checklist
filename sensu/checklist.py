6bb0c2f7170a09cab4e1a759d21deadfc6bf7817
Flier Lu <flier.lu@gmail.com>
2014-01-17 11:49:48 +0800

    add setup scripts

---
+++ sensu/checklist.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = 'flier'
+
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import ez_setup
+ez_setup.use_setuptools()
+
+__author__ = 'Flier Lu <flier.lu@gmail.com>'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sensu-checklist",
+    version="0.8",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'sensu-checklist = sensu.checklist:run'
+        ]
+    },
+
+    install_requires=[],
+
+    author="Flier Lu",
+    author_email="flier.lu@gmail.com",
+    description="Template based checklist generator and runner for Sensu",
+    license="MIT",
+    keywords="sensu checklist monitoring devops",
+    url="https://github.com/flier/sensu-checklist",
+)
